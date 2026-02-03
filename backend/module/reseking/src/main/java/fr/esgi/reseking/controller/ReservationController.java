6c85f51cd62659d0fbb441a416537c5807500fbe
SidMyGES <sarifi@myges.fr>
2026-02-03 15:44:00 +0100

    added a GET endpoint to retrieve all reservations

--- backend/module/reseking/src/main/java/fr/esgi/reseking/controller/ReservationController.java
+++ backend/module/reseking/src/main/java/fr/esgi/reseking/controller/ReservationController.java
@@ -4,9 +4,12 @@ import fr.esgi.reseking.controller.dto.ReservationDTO;
 import fr.esgi.reseking.model.ReservationEntity;
 import fr.esgi.reseking.repository.ReservationRepository;
 import fr.esgi.reseking.util.ReservationMapper;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ReservationController {
@@ -21,5 +24,11 @@ public class ReservationController {
     public void add(@RequestBody ReservationDTO reservation) {
         final ReservationEntity reservationEntity = ReservationMapper.map(reservation);
         reservationRepository.save(reservationEntity);
+    }
+
+    @GetMapping("/reservations")
+    public List<ReservationDTO> getAll() {
+        final List<ReservationEntity> reservationsEntities = reservationRepository.findAll();
+        return reservationsEntities.stream().map(ReservationMapper::map).toList();
     }
 }
