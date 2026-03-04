03d95b9cf6c90603762f334e6553b79bd608486e
yannick <yannick987xd@gmail.com>
2026-03-04 04:13:24 +0100

    "add token in spot request"

--- frontend/Reseking/src/features/parking/parking.api.ts
+++ frontend/Reseking/src/features/parking/parking.api.ts
@@ -1,6 +1,17 @@
 import type { ParkingSpot } from "./parking.types";
-
+import { tokenStorage } from "../auth/auth.api";
+const BASE_URL = `${import.meta.env.VITE_API_URL}/spots`;
+const getAuthHeaders = (): HeadersInit => {
+  const token = tokenStorage.get();
+  return {
+    "Content-Type": "application/json",
+    ...(token && { Authorization: `Bearer ${token}` }),
+  };
+};
 export async function fetchParkingSpots(): Promise<ParkingSpot[]> {
-  const response = await fetch("http://localhost:8080/api/spots");
-  return response.json();
+   const response = await fetch(BASE_URL, {
+      headers: getAuthHeaders(),
+    });
+    if (!response.ok) throw new Error("Erreur chargement");
+    return response.json();
 }
